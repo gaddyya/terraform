e28cc8e1d90bd69c4c8ed6220eefd9be6445720a
Roman Dan <gaddyya@gmail.com>
2022-08-06 19:53:44 +0300

    commit message

---
+++ PROD/variable.tf
@@ -0,0 +1,56 @@
+variable "LOCATION" {}
+variable "RESOURCE_NAME_PREFIX" {}
+variable "ENV" {}
+variable "SQL_SERVER_ADMINISTRATOR_LOGIN" {}
+variable "SQL_SERVER_ADMINISTRATOR_PASSWORD" {}
+
+### Variables that have some default values but also acn be specified if you'd like.
+variable "sql_server_version" {
+  default = "12.0"
+}
+variable "sql_server_connection_policy" {
+  default = "Default"
+}
+variable "sql_server_firewall_rules" {
+  type = map(any)
+  default = {
+    allow_azure_services = {
+      start_ip_address = "0.0.0.0"
+      end_ip_address   = "0.0.0.0"
+    }
+  }
+}
+variable "edition" {
+  default = "Basic"
+}
+variable "collation" {
+  default = "SQL_Latin1_General_CP1_CI_AS"
+}
+variable "max_size_gb" {
+  default = 4
+}
+variable "zone_redundant" {
+  type    = bool
+  default = false
+}
+variable "kind" {
+  default = "Windows"
+}
+variable "sku_tier" {
+  default = "Standard"
+}
+variable "sku_size" {
+  default = "S1"
+}
+
+variable "os_type" {
+  default = "Windows"
+}
+
+variable "sku_name" {
+  default = "P1v2"
+}
+
+variable "connection_string_name" {
+  default = "MyDbConnection"
+}
---
+++ QA/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.78.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-adolearn-dan-tfstate"
+    storage_account_name = "storageaccadolearndan"
+    container_name       = "tfstate"
+    key                  = "qa.tfstate"
+  }
+}
